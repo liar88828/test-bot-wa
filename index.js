e9a7844768597941429230fd7f58a88169bb4da1
liar88828 <liar88828@gmail.com>
2023-10-26 22:53:42 +0700

    deploy for vercel

---
+++ index.js
@@ -0,0 +1,64 @@
+const qrcode = require( 'qrcode-terminal' );
+const { Client } = require( 'whatsapp-web.js' );
+
+const express = require( "express" );
+
+const app = express();
+
+
+
+const PORT = process.env.PORT || 5000;
+const client = new Client( {
+} );
+
+
+client.on( 'ready', () =>
+{
+  console.log( 'Client is ready!' );
+} );
+
+client.on( 'message', message =>
+{
+  if ( message.body.toLowerCase() === 'p' )
+  {
+    client.sendMessage( message.from, 'apakah yang bisa saya bantu ?' );
+  }
+
+  if ( message.body === 'koe neng endi' )
+  {
+    client.sendMessage( message.from, 'indonesia' );
+  }
+
+  if ( message.body !== 'baik' )
+  {
+    console.log( message.from )
+    console.log( message.body )
+    client.sendMessage( message.from, 'baik' );
+  }
+} );
+
+
+app.get( "/", ( req, res ) =>
+{
+  client.on( 'qr', qr =>
+  {
+    // qrcode.generate( qr, { small: true } );
+
+
+
+    res.send( `deploy for vercel : ${ qr }` );
+  } );
+
+} );
+
+app.listen( 5000, () =>
+{
+  client.initialize();
+  console.log( "Running on port 5000." );
+} );
+
+
+
+// Export the Express API
+module.exports = app;
+
